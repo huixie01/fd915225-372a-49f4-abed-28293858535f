7ea29b4d788529845a645685a7ef7195d74952cb
hui xie <huixie01@users.noreply.github.com>
2020-12-23 16:18:15 -0500

    update view

--- view/js/script.js
+++ view/js/script.js
@@ -1,1 +1,17 @@
-var item = require('express');
+function viewList(item) {
+	var newitem = document.createElement("li");
+	newitem = document.getElementById("list").innerHTML=item;
+	
+	if (newitem == "")
+		alert("Please provide the valid input");
+	else
+		document.appendChild(newitem);
+}
+
+function inputList() {
+	
+		var x = document.getElementById("input").value;
+		console.log("input:"+x);
+		viewList(x);
+	
+}
